ebc5d790160c007992e56a4848a56d0894af2df4
JulianoF <juliano1@live.ca>
2024-02-29 12:12:45 -0500

    unknown jar packaging isssue still persits

--- Clustering-Testing/src/main/java/com/juliano/clustering/App.java
+++ Clustering-Testing/src/main/java/com/juliano/clustering/App.java
@@ -1,13 +1,28 @@
 package com.juliano.clustering;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+
+import net.sf.javaml.clustering.Clusterer;
+import net.sf.javaml.clustering.KMeans;
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.tools.data.FileHandler;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        /* Load a dataset */
+        Dataset data = FileHandler.loadDataset(new File("datasets/iris.data"), 4, ",");
+        /*
+         * Create a new instance of the KMeans algorithm, with no options
+         * specified. By default this will generate 4 clusters.
+         */
+        Clusterer km = new KMeans();
+        /*
+         * Cluster the data, it will be returned as an array of data sets, with
+         * each dataset representing a cluster
+         */
+        Dataset[] clusters = km.cluster(data);
+        System.out.println("Cluster count: " + clusters.length);
     }
 }
