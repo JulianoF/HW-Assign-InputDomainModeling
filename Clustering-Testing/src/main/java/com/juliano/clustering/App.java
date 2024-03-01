5683feb9bba98071aa9c0a8504a7dde7e1659d86
JulianoF <juliano1@live.ca>
2024-02-29 19:43:58 -0500

    update

--- Clustering-Testing/src/main/java/com/juliano/clustering/App.java
+++ Clustering-Testing/src/main/java/com/juliano/clustering/App.java
@@ -33,34 +33,40 @@ public class App {
         Dataset[] clusters2 = adapt.cluster(data);
         Dataset[] clusters3 = somTest.cluster(data);
 
-        System.out.println("\n******************************************************");
-        System.out.println("KMEAN Cluster count: " + clusters1.length);
-        System.out.println("AQBC Cluster count: " + clusters2.length);
-        System.out.println("SOM Cluster count: " + clusters3.length);
-        System.out.println("******************************************************\n");
+        // System.out.println(clusters2[0]);
 
-        Clusterer km3 = new KMeans(3);
-        Clusterer km4 = new KMeans(4);
         /*
          * Cluster the data, we will create 3 and 4 clusters.
          */
-        Dataset[] clusters4 = km3.cluster(data);
-        Dataset[] clusters5 = km4.cluster(data);
 
         ClusterEvaluation aic = new AICScore();
         ClusterEvaluation bic = new BICScore();
         ClusterEvaluation sse = new SumOfSquaredErrors();
 
-        double aicScore3 = aic.score(clusters4);
-        double bicScore3 = bic.score(clusters4);
-        double sseScore3 = sse.score(clusters4);
+        double aicScore3 = aic.score(clusters1);
+        double bicScore3 = bic.score(clusters1);
+        double sseScore3 = sse.score(clusters1);
 
-        double aicScore4 = aic.score(clusters5);
-        double bicScore4 = bic.score(clusters5);
-        double sseScore4 = sse.score(clusters5);
+        double aicScore4 = aic.score(clusters2);
+        double bicScore4 = bic.score(clusters2);
+        double sseScore4 = sse.score(clusters2);
 
-        System.out.println("AIC score: " + aicScore3 + "\t" + aicScore4);
-        System.out.println("BIC score: " + bicScore3 + "\t" + bicScore4);
-        System.out.println("Sum of squared errors: " + sseScore3 + "\t" + sseScore4);
+        double aicScore5 = aic.score(clusters3);
+        double bicScore5 = bic.score(clusters3);
+        double sseScore5 = sse.score(clusters3);
+
+        System.out.println(
+                "\n******************************************************************************************");
+        System.out.println("KMEAN Cluster count: " + clusters1.length);
+        System.out.println("AQBC Cluster count: " + clusters2.length);
+        System.out.println("SOM Cluster count: " + clusters3.length + "\n");
+        System.out.println("\t\t\t KMEAN \t\t\tAQBC \t\t\tSOM ");
+        System.out.println("AIC score:\t\t " + aicScore3 + "\t" + aicScore4 + "\t" + aicScore5);
+        System.out.println("BIC score:\t\t " + bicScore3 + "\t" + bicScore4 + "\t" + bicScore5);
+        System.out.println("Sum of squared errors:   " + sseScore3 + "\t" + sseScore4 + "\t" + sseScore5);
+
+        System.out.println(
+                "********************************************************************************************\n");
+
     }
 }
