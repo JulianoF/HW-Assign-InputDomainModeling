5a81fa2c487dd4817ec42851c8d2b83711fe184a
JulianoF <juliano1@live.ca>
2024-03-04 16:13:58 -0500

    Add Comparsion metrics

--- Clustering-Testing/src/main/java/com/juliano/clustering/App.java
+++ Clustering-Testing/src/main/java/com/juliano/clustering/App.java
@@ -5,12 +5,14 @@ import java.io.File;
 import net.sf.javaml.clustering.evaluation.AICScore;
 import net.sf.javaml.clustering.evaluation.BICScore;
 import net.sf.javaml.clustering.evaluation.ClusterEvaluation;
+import net.sf.javaml.clustering.evaluation.SumOfAveragePairwiseSimilarities;
 import net.sf.javaml.clustering.evaluation.SumOfSquaredErrors;
 
 import net.sf.javaml.clustering.AQBC;
 import net.sf.javaml.clustering.SOM;
 import net.sf.javaml.clustering.Clusterer;
 import net.sf.javaml.clustering.KMeans;
+import net.sf.javaml.clustering.FarthestFirst;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.tools.data.FileHandler;
 
@@ -18,42 +20,46 @@ public class App {
     public static void main(String[] args) throws Exception {
         /* Load a dataset */
         Dataset data = FileHandler.loadDataset(new File("datasets/iris.data"), 4, ",");
-        /*
-         * Create a new instance of the KMeans algorithm, with no options
-         * specified. By default this will generate 4 clusters.
-         */
+
         Clusterer km = new KMeans();
+        // Clusterer far = new FarthestFirst();
         Clusterer adapt = new AQBC();
         Clusterer somTest = new SOM();
-        /*
-         * Cluster the data, it will be returned as an array of data sets, with
-         * each dataset representing a cluster
-         */
+
+        long startTime = System.nanoTime();
         Dataset[] clusters1 = km.cluster(data);
+        long endTime = System.nanoTime();
+        long durationKMEAN = endTime - startTime;
+
+        startTime = System.nanoTime();
         Dataset[] clusters2 = adapt.cluster(data);
+        endTime = System.nanoTime();
+        long durationAQBC = endTime - startTime;
+
+        startTime = System.nanoTime();
         Dataset[] clusters3 = somTest.cluster(data);
-
-        // System.out.println(clusters2[0]);
-
-        /*
-         * Cluster the data, we will create 3 and 4 clusters.
-         */
+        endTime = System.nanoTime();
+        long durationSOM = endTime - startTime;
 
         ClusterEvaluation aic = new AICScore();
         ClusterEvaluation bic = new BICScore();
         ClusterEvaluation sse = new SumOfSquaredErrors();
+        ClusterEvaluation saps = new SumOfAveragePairwiseSimilarities();
 
         double aicScore3 = aic.score(clusters1);
         double bicScore3 = bic.score(clusters1);
         double sseScore3 = sse.score(clusters1);
+        double sapScore3 = saps.score(clusters1);
 
         double aicScore4 = aic.score(clusters2);
         double bicScore4 = bic.score(clusters2);
         double sseScore4 = sse.score(clusters2);
+        double sapScore4 = saps.score(clusters2);
 
         double aicScore5 = aic.score(clusters3);
         double bicScore5 = bic.score(clusters3);
         double sseScore5 = sse.score(clusters3);
+        double sapScore5 = saps.score(clusters3);
 
         System.out.println(
                 "\n******************************************************************************************");
@@ -63,8 +69,12 @@ public class App {
         System.out.println("\t\t\t KMEAN \t\t\tAQBC \t\t\tSOM ");
         System.out.println("AIC score:\t\t " + aicScore3 + "\t" + aicScore4 + "\t" + aicScore5);
         System.out.println("BIC score:\t\t " + bicScore3 + "\t" + bicScore4 + "\t" + bicScore5);
+        System.out.println("Pairwise Similarities:\t " + sapScore3 + "\t" + sapScore4 + "\t" + sapScore5);
         System.out.println("Sum of squared errors:   " + sseScore3 + "\t" + sseScore4 + "\t" + sseScore5);
-
+        System.out.println(
+                "Execution time in MS:    " + durationKMEAN / 1000000 + "ms \t\t\t" + durationAQBC / 100000
+                        + "ms \t\t\t"
+                        + durationSOM / 100000 + "ms");
         System.out.println(
                 "********************************************************************************************\n");
 
